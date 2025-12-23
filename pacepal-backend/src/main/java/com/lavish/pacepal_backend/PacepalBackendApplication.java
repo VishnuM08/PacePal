4839517271d1dff6a77e7adfae1486552391ca12
Vishnu Magesh <vishnu.magesh@cognizant.com>
2025-12-23 14:17:16 +0530

    Added Spring Boot 3 skeleton with Java 17

---
+++ pacepal-backend/src/main/java/com/lavish/pacepal_backend/PacepalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lavish.pacepal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacepalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacepalBackendApplication.class, args);
+	}
+
+}
---
+++ pacepal-backend/src/test/java/com/lavish/pacepal_backend/PacepalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavish.pacepal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacepalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
